c3dab95443b7fb60ca87222d73eb8412d8a18a02
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2024-07-23 14:57:40 +0500

    feat: add top head link list

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,21 +1,28 @@
+'use client';
 import { ChangeLocale } from "@/features";
 import { BurgerMenu } from "@/widgets";
 import { Navbar } from "@/widgets/Header/Navbar";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 export const Header = () => {
+
+  const params = useParams();
+
   return (
-    <header className="relative  flex flex-col   w-full ">
-      <section className="bg-red-950 min-h-10 flex gap-6 justify-end gap-6 pr-3 ">
-        <section className="flex gap-3">
-          <Link href="/main">Обучающимся</Link>
-          <Link href="/main">Международный отдел</Link>
-          <Link href="/main">Выпусникам</Link>
-          <Link href="/main">AIS</Link>
-          <Link href="/main">Abai IT</Link>
+    <header className="relative  flex flex-col gap-10 w-full ">
+      <section className="bg-red-950 min-h-10 flex gap-20 justify-end  pr-3 ">
+        <section className="flex gap-10 text-white items-center">
+          <Link href={`/${params.locale}/main`}>Обучающимся</Link>
+          <Link href={`/${params.locale}/main`}>Международный отдел</Link>
+          <Link href={`/${params.locale}/main`}>Выпусникам</Link>
+          <Link href={`/${params.locale}/main`}>AIS</Link>
+          <Link href={`/${params.locale}/main`}>Abai IT</Link>
         </section>
-        <ChangeLocale />
-        <BurgerMenu />
+        <section className="flex items-center">
+          <ChangeLocale />
+          <BurgerMenu />
+        </section>
       </section>
       <Navbar />
     </header>
