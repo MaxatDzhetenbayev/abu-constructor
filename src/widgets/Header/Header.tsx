c7e0b1463f5fb5ec3f00ef5b2b5528e3d24ed233
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2024-07-23 10:00:44 +0500

    update:header

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,13 +1,18 @@
 import { ChangeLocale } from "@/features";
 import { BurgerMenu } from "@/widgets";
 import { Navbar } from "@/widgets/Header/Navbar";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className="relative  flex flex-col   w-full ">
-      <section className="bg-red-950 min-h-10 flex gap-6 justify-end pr-3 ">
-        <section>
-          sdfsf
+      <section className="bg-red-950 min-h-10 flex gap-6 justify-end gap-6 pr-3 ">
+        <section className="flex gap-3">
+          <Link href="/main">Обучающимся</Link>
+          <Link href="/main">Международный отдел</Link>
+          <Link href="/main">Выпусникам</Link>
+          <Link href="/main">AIS</Link>
+          <Link href="/main">Abai IT</Link>
         </section>
         <ChangeLocale />
         <BurgerMenu />
