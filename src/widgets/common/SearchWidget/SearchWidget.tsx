bd588ef1c90c4fc69f2d3907b5cb1a624aac10f4
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2024-10-14 03:34:36 +0500

    fix: Вложенные кнопки

--- src/widgets/common/SearchWidget/SearchWidget.tsx
+++ src/widgets/common/SearchWidget/SearchWidget.tsx
@@ -1,27 +1,23 @@
-"use client"
-import React from 'react'
-import { SearchBar } from '@/features/search-bar'
-import { Button, Dialog, DialogTrigger, DialogContent } from '@/shared/ui'
-import { DialogTitle } from '@radix-ui/react-dialog'
-import { SearchIcon } from 'lucide-react'
-import { useTranslations } from 'next-intl'
+"use client";
+import React from "react";
+import { SearchBar } from "@/features/search-bar";
+import { Button, Dialog, DialogTrigger, DialogContent } from "@/shared/ui";
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { SearchIcon } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 export const SearchWidget = () => {
+  const t = useTranslations("search");
 
-    const t = useTranslations("search");
-
-    return (
-        <Dialog>
-            <DialogTrigger >
-                <Button>
-                    <SearchIcon className='text-white w-10 cursor-pointer' />
-                </Button>
-            </DialogTrigger>
-            <DialogContent className='sm:max-w-[40%] max-h-[95%] overflow-y-auto'>
-                <DialogTitle>{t("title")}</DialogTitle>
-                <SearchBar />
-            </DialogContent>
-        </Dialog>
-
-    )
-}
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <SearchIcon className="text-white w-10 cursor-pointer" />
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[40%] max-h-[95%] overflow-y-auto">
+        <DialogTitle>{t("title")}</DialogTitle>
+        <SearchBar />
+      </DialogContent>
+    </Dialog>
+  );
+};
