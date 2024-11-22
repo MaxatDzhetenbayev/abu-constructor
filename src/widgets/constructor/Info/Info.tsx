fee10078479cd9d92f6478c175f0323df4f34983
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2024-11-22 08:01:18 +0500

    change(edit): new info

--- src/widgets/constructor/Info/Info.tsx
+++ src/widgets/constructor/Info/Info.tsx
@@ -16,29 +16,28 @@ function Info({
   options: any;
   locale: string;
 }) {
-
-
-
   return (
-    <section className="flex flex-col">
+    <section className="flex  flex-col">
       <h2 className="text-2xl font-bold text-[#690000]">
         {content[locale]?.title}
       </h2>
-      <section className="flex flex-col md:flex-row  gap-4 shadow-xl rounded-md  p-5 bg-white mt-4 ">
-        <section className="flex flex-col gap-1 w-[100%] md:w-[35%]">
-          <section className="relative w-[100%] h-[350px]">
-            <Image
-              src={`${backendImageUrl}${content.image}`}
-              fill
-              objectFit="cover"
-              objectPosition="top"
-              alt="image"
-              className="rounded-md"
-            />
-          </section>
-          <h2 className="text-center font-bold text-[20px]">{content[locale]?.full_name}</h2>
+      <section className="flex p-10 flex-row  gap-14 shadow-[0_0_30px_0px_rgba(0,0,0,0.05)] rounded-2xl  bg-white mt-4 ">
+        <section className="relative w-[400px] h-[363px]">
+          <Image
+            src={`${backendImageUrl}${content.image}`}
+            fill
+            objectFit="cover"
+            alt="image"
+            className="rounded-2xl"
+          />
         </section>
-        <section className="w-[100%] md:w-[65%] quill-content" dangerouslySetInnerHTML={{ __html: content[locale].content }}></section>
+        <section className="flex flex-col gap-10">
+          <h2 className="font-bold text-2xl">{content[locale]?.full_name}</h2>
+          <div
+            className="quill-content"
+            dangerouslySetInnerHTML={{ __html: content[locale].content }}
+          ></div>
+        </section>
       </section>
     </section>
   );
