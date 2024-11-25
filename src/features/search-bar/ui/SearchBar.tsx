a40b40ce6909bd846bae83abc0c15bce706cc7b4
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2024-11-25 07:38:13 +0500

    fix: Адаптив пути

--- src/features/search-bar/ui/SearchBar.tsx
+++ src/features/search-bar/ui/SearchBar.tsx
@@ -1,45 +1,53 @@
-"use client"
-import React, { useState } from 'react'
-import { useSearch } from '../model/useSearch'
-import { useParams } from 'next/navigation'
-import { Input } from '@/shared/ui'
-import Link from 'next/link'
-import { Badge } from '@/shared/ui/badge'
-import { useTranslations } from 'next-intl'
+"use client";
+import React, { useState } from "react";
+import { useSearch } from "../model/useSearch";
+import { useParams } from "next/navigation";
+import { Input } from "@/shared/ui";
+import Link from "next/link";
+import { Badge } from "@/shared/ui/badge";
+import { useTranslations } from "next-intl";
 
 export const SearchBar = () => {
+  const [query, setQuery] = useState("");
+  const locale = useParams().locale as string;
+  const t = useTranslations("search");
 
-    const [query, setQuery] = useState('')
-    const locale = useParams().locale as string
-    const t = useTranslations("search");
+  const { data, isLoading } = useSearch(query, locale);
 
-    const { data, isLoading } = useSearch(query, locale)
-
-
-    return (
-        <section>
-            <Input placeholder="Напишите для поиска" className='w-48' label="" value={query} onChange={(e) => setQuery(e.target.value)} />
-            {isLoading
-                ? <p>Loading...</p>
-                : (
-                    <ul>
-                        {
-                            data?.map((item: any) => {
-                                console.log(item)
-                                return <li key={item.id} >
-                                    <Link href={`/${locale}/${item.slug}`} className='flex items-center justify-between p-2 hover:bg-muted rounded-md'>
-                                        <span className='flex flex-col gap-1'>
-                                            <sub>{item.all_title}</sub>
-                                            <h3 className='quill-content ql-size-large' >{item.title}</h3>
-                                        </span>
-                                        <Badge variant="outline">{t(item.type)}</Badge>
-                                    </Link>
-                                </li>
-                            })
-                        }
-                    </ul>
-                )
-            }
-        </section >
-    )
-}
+  return (
+    <section>
+      <Input
+        placeholder="Напишите для поиска"
+        className="w-48"
+        label=""
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {data?.map((item: any) => {
+            console.log(item);
+            return (
+              <li key={item.id}>
+                <Link
+                  href={`/${locale}/${item.slug}`}
+                  className="flex items-center justify-between p-2 hover:bg-muted rounded-md"
+                >
+                  <span className="flex flex-col gap-1">
+                    <p className="text-[12px]">{item.all_title}</p>
+                    <h3 className="quill-content ql-size-large">
+                      {item.title}
+                    </h3>
+                  </span>
+                  <Badge variant="outline">{t(item.type)}</Badge>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </section>
+  );
+};
