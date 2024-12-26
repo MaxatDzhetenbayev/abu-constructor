a733366675a7788c7624ff70701055d21fb0b91c
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2024-12-26 14:34:20 +0500

    feat: Тултип на карточки, если текст не влезает в 2 строки

--- src/entities/Card/index.ts
+++ src/entities/Card/index.ts
@@ -1,3 +1,1 @@
 export { CardBase } from "./ui/CardBase";
-export { CardWithModal } from "./ui/CardWithModal";
-export { CardHoverAnimation } from "./ui/CardHoverAnimation";
--- src/entities/Card/ui/CardBase.tsx
+++ src/entities/Card/ui/CardBase.tsx
@@ -6,6 +6,7 @@ import React from "react";
 
 import { backendImageUrl } from "@/shared/lib/constants";
 import { Dialog, DialogContent, DialogTrigger, MoreArrow } from "@/shared/ui";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/shared/ui/tooltip";
 import { ICard } from "@/widgets/constructor/Cards/model/Cards.interface";
 
 type ICardWithPath = ICard & Record<"currentPath", string>
@@ -28,54 +29,74 @@ export const CardBase = ({
     ? { href: `${backendImageUrl}/${content.file}`, target: "_blank" }
     : content.link && { href: `${currentPath}/${content.link}` };
 
+
+
   return (
-    <WrapperComponent
-      {...linkProps}
-      className={clsx(
-        "flex grow gap-5 rounded-2xl shadow-md overflow-hidden p-[20px] group hover:bg-abu_primary_hover transition-colors duration-500",
-        styles,
-        variant === "with_modal"
-          ? "flex-col"
-          : "justify-between items-center"
-      )}
-    >
-      {variant !== "with_modal" ? (
-        <>
-          <Heading title={title} />
-          <MoreArrow width={36} height={24} />
-        </>
-      ) : (
-        <div className="h-full flex flex-col">
-          {content.image && (
-            <div className="relative w-full h-[260px] ">
-              <Image
-                src={`${backendImageUrl}${content.image}`}
-                fill
-                className="rounded-xl"
-                alt="Изображение"
-              />
-            </div>
-          )}
-          <Dialog>
-            <DialogTrigger asChild>
-              <button className="grow  flex flex-col justify-between">
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <WrapperComponent
+            {...linkProps}
+            className={clsx(
+              "flex grow gap-5 rounded-2xl shadow-md overflow-hidden p-[20px] group hover:bg-abu_primary_hover transition-colors duration-500",
+              styles,
+              variant === "with_modal"
+                ? "flex-col"
+                : "justify-between items-center"
+            )}
+          >
+            {variant !== "with_modal" ? (
+              <>
                 <Heading title={title} />
-                <div className="flex items-center gap-5 mt-[15px]">
-                  <p className="group-hover:text-white">Подробнее</p>
-                  <MoreArrow width={17} height={13} />
-                </div>
-              </button>
-            </DialogTrigger>
-            <DialogContent className="max-h-[80%] overflow-auto max-w-[90%] [@media(min-width:1180px)]:max-w-[50%]">
-              <div
-                className={`quill-content`}
-                dangerouslySetInnerHTML={{ __html: description }}
-              ></div>
-            </DialogContent>
-          </Dialog>
-        </div>
-      )}
-    </WrapperComponent>
+                <MoreArrow width={36} height={24} />
+              </>
+            ) : (
+              <div className="h-full flex flex-col">
+                {content.image && (
+                  <div className="relative w-full h-[260px]">
+                    <Image
+                      src={`${backendImageUrl}${content.image}`}
+                      fill
+                      className="rounded-xl"
+                      alt="Изображение"
+                    />
+                  </div>
+                )}
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <button className="grow  flex flex-col justify-between">
+                      <Heading title={title} />
+                      <div className="flex items-center gap-5 mt-[15px]">
+                        <p className="group-hover:text-white">Подробнее</p>
+                        <MoreArrow width={17} height={13} />
+                      </div>
+                    </button>
+                  </DialogTrigger>
+                  <DialogContent className="max-h-[80%] overflow-auto max-w-[90%] [@media(min-width:1180px)]:max-w-[50%]">
+                    <div
+                      className={`quill-content`}
+                      dangerouslySetInnerHTML={{ __html: description }}
+                    ></div>
+                  </DialogContent>
+                </Dialog>
+              </div>
+            )}
+          </WrapperComponent>
+        </TooltipTrigger>
+        <TooltipContent>
+          {
+            title.length > 28 && (
+              <h2
+                className="text-calc-xl grow font-bold p-1"
+              >
+                {title}
+              </h2>
+            )
+          }
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+
   );
 };
 
