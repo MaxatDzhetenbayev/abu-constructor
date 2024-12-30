e76b5f211bf640e3d9ad690b6ee3952cdebcb1ad
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2024-12-30 16:38:53 +0500

    feat(options): Пока что редактируется

--- src/app/[locale]/admin/(withSidebar)/news/page.tsx
+++ src/app/[locale]/admin/(withSidebar)/news/page.tsx
@@ -1,7 +1,41 @@
+"use client"
+import { useParams } from 'next/navigation'
 import React from 'react'
 
+import { INews } from '@/entities/news/model/types'
+import { backendUrl } from '@/shared/lib/constants'
+
+import { useQuery } from '@tanstack/react-query'
+
+
+
+
 export default function NewsPage() {
+
+
+    const lang = useParams().locale as string
+
+    const { data: news } = useQuery<INews[]>({
+        queryKey: ['news'],
+        queryFn: async () => {
+            const res = await fetch(`${backendUrl}/news`)
+            return res.json()
+        }
+    })
+
+
     return (
-        <div>page</div>
+        <section>
+            <h1>Новости</h1>
+            <ul>
+                {news?.map((item) => (
+                    <li key={item.id}>
+                        {
+                            item.title[lang]
+                        }
+                    </li>
+                ))}
+            </ul>
+        </section>
     )
 }
--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -1,12 +1,12 @@
-import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-import * as React from "react";
-
 import clsx from "clsx";
 import { Loader2 } from "lucide-react";
+import * as React from "react";
+
+import { Slot } from "@radix-ui/react-slot";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex  items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -60,7 +60,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <Comp
         type={type}
-        className={clsx(buttonVariants({ variant, size, className }))}
+        className={clsx("", buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       >
