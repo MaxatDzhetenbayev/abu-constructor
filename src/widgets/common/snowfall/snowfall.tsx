784e61ba4cd59eb6e1d44b0fdcd2697503d98f1f
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2024-12-30 16:38:19 +0500

    update: количество снега

--- src/widgets/common/snowfall/snowfall.tsx
+++ src/widgets/common/snowfall/snowfall.tsx
@@ -5,7 +5,7 @@ import Snowfall from 'react-snowfall'
 export const SnowfallView = () => {
     return (
         <div className='fixed z-30 top-0 left-0 bottom-0 right-0 w-full h-full pointer-events-none'>
-            <Snowfall />
+            <Snowfall snowflakeCount={40} />
         </div>
     )
 }
