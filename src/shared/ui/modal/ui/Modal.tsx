7467313a4e1f9e154d7695ab9f668c6442f5503c
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-01-05 08:53:02 +0500

    isFullWidth

--- src/shared/ui/modal/ui/Modal.tsx
+++ src/shared/ui/modal/ui/Modal.tsx
@@ -1,36 +1,51 @@
-import clsx from 'clsx';
-import React from 'react'
+import clsx from "clsx";
+import React from "react";
 
-import { Dialog, DialogContent, DialogHeader, DialogTrigger } from '../../dialog'
-import { DialogTitle } from '@radix-ui/react-dialog';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "../../dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 interface ModalProps {
-    modalSlot: React.ReactNode | string
-    headerSlot?: React.ReactNode | string;
-    children: React.ReactNode;
-    isWfull?: boolean;
+  modalSlot: React.ReactNode | string;
+  headerSlot?: React.ReactNode | string;
+  children: React.ReactNode;
+  isWfull?: boolean;
 }
 
-export const Modal = ({ modalSlot, children, headerSlot }: ModalProps) => {
-    return (
-        <Dialog>
-            <DialogTrigger asChild>
-                {children}
-            </DialogTrigger>
-            <DialogContent className=
-            "max-h-[80%] overflow-auto max-w-[90%] [@media(min-width:1180px)]:max-w-[50%]"
-            >
-                <DialogHeader>
-                    <DialogTitle className={clsx(!headerSlot ? "opacity-0" : "")}>{headerSlot}</DialogTitle>
-                </DialogHeader>
-                {
-                    typeof modalSlot === 'string'
-                        ? (
-                            <div className={`quill-content`} dangerouslySetInnerHTML={{ __html: modalSlot }}></div>
-                        )
-                        : modalSlot
-                }
-            </DialogContent>
-        </Dialog>
-    )
-}
+export const Modal = ({
+  modalSlot,
+  children,
+  headerSlot,
+  isWfull,
+}: ModalProps) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent
+        className={clsx(
+          isWfull
+            ? "max-w-[90%]"
+            : "max-h-[80%] overflow-auto max-w-[90%] [@media(min-width:1180px)]:max-w-[50%]"
+        )}
+      >
+        <DialogHeader>
+          <DialogTitle className={clsx(!headerSlot ? "opacity-0" : "")}>
+            {headerSlot}
+          </DialogTitle>
+        </DialogHeader>
+        {typeof modalSlot === "string" ? (
+          <div
+            className={`quill-content`}
+            dangerouslySetInnerHTML={{ __html: modalSlot }}
+          ></div>
+        ) : (
+          modalSlot
+        )}
+      </DialogContent>
+    </Dialog>
+  );
+};
