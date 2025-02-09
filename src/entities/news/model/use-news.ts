36dda88b9e76f2393a53fb8ad52fb7b2ac82ee7d
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-02-09 13:09:31 +0500

    update: Фильтрация новостей по критериям

--- src/entities/news/model/use-news.ts
+++ src/entities/news/model/use-news.ts
@@ -7,15 +7,29 @@ import { useQuery } from "@tanstack/react-query";
 interface IUseNewsProps {
   limit?: number;
   offset?: number;
+  search?: string;
+  startDate?: string;
+  endDate?: string;
 }
 
-export const useNews = ({ limit, offset }: IUseNewsProps) => {
+export const useNews = ({
+  limit,
+  offset,
+  startDate,
+  endDate,
+  search,
+}: IUseNewsProps) => {
   return useQuery<INewsResponse>({
-    queryKey: ["news", limit, offset],
+    queryKey: ["news", limit, offset, search, startDate, endDate],
     queryFn: async () => {
-      const response = await fetch(
-        `${backendUrl}/news?limit=${limit}&offset=${offset}`
-      );
+      const params = new URLSearchParams();
+      if (limit) params.append("limit", String(limit));
+      if (offset) params.append("offset", String(offset));
+      if (search) params.append("query", search);
+      if (startDate) params.append("startDate", startDate);
+      if (endDate) params.append("endDate", endDate);
+
+      const response = await fetch(`${backendUrl}/news?${params.toString()}`);
       return response.json();
     },
     refetchOnWindowFocus: false,
