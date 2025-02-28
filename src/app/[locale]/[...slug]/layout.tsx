519bf8f80a9688b46cd4088b0952112cd4fdd69d
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-02-28 06:34:29 +0500

    change: Адаптивность лэйаута

--- src/app/[locale]/[...slug]/layout.tsx
+++ src/app/[locale]/[...slug]/layout.tsx
@@ -12,7 +12,7 @@ const socials = [
   "/enbek/socials/Instagram.png",
   "/enbek/socials/Telegram.png",
   "/enbek/socials/TikTok.png",
-]
+];
 
 export default async function Layout({
   children,
@@ -24,7 +24,7 @@ export default async function Layout({
   return (
     <section>
       <Header />
-      <section className="container mx-auto grid grid-cols-4 gap-10 mt-10">
+      <section className="container px-4 md:0 mx-auto grid grid-cols-4 gap-10 my-10">
         <LayoutWrapper>
           <BreadCrumbs locale={params.locale} slug={params.slug} />
           <section className="flex flex-col h-full ">
@@ -43,21 +43,25 @@ export default async function Layout({
         <div className="flex flex-col gap-5 items-center">
           <h2 className="text-white text-3xl font-bold">БІЗГЕ ТІРКЕЛ!</h2>
           <div className="flex gap-4">
-            {socials.map(s => <a><Image src={s} alt={s} width={40} height={40}
-            /></a>)}
+            {socials.map((s) => (
+              <a>
+                <Image src={s} alt={s} width={40} height={40} />
+              </a>
+            ))}
           </div>
         </div>
         <div className="flex flex-col gap-4 text-white">
-          <h2 className="text-2xl font-bold">
-            Наши контакты
-          </h2>
+          <h2 className="text-2xl font-bold">Наши контакты</h2>
           <div className="flex gap-3">
             <MapPin />
             <span>Абай облысы, Семей қаласы, Т.Ұранхаев көшесі 53</span>
           </div>
           <div className="flex gap-3">
             <Phone />
-            <span><a href="tel:24-93-09">24-93-09</a>, <a href="tel:+7 700 956 98 10">+7 700 956 98 10</a></span>
+            <span>
+              <a href="tel:24-93-09">24-93-09</a>,{" "}
+              <a href="tel:+7 700 956 98 10">+7 700 956 98 10</a>
+            </span>
           </div>
         </div>
       </footer>
--- src/shared/ui/layout-wrapper.tsx
+++ src/shared/ui/layout-wrapper.tsx
@@ -4,11 +4,10 @@ import clsx from "clsx";
 import React, { ReactNode } from "react";
 
 export const LayoutWrapper = ({ children }: { children: ReactNode }) => {
-
   return (
     <main
       className={clsx(
-        "col-span-3 min-h-[100svh] max-w-[1200px] mx-auto flex flex-col lg:px-0 px-3 gap-10 my-10 2md:mb-40 max-2md:mt-[120px] 2md:relative ",
+        "col-span-4 md:col-span-3 min-h-[100svh] max-w-[1200px] mx-auto flex flex-col gap-10 my-10  max-2md:mt-[120px] 2md:relative "
       )}
     >
       {children}
