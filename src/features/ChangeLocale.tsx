3e5724f79c665259d9f78c01c32375e22d73b20b
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2025-03-03 10:51:22 +0500

    fix: Почина перекидываение вниз при смене языка

--- src/features/ChangeLocale.tsx
+++ src/features/ChangeLocale.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useParams, usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 import {
   Select,
@@ -15,16 +16,23 @@ export const ChangeLocale = () => {
   const path = usePathname().split("/");
   const slug = path.slice(2, path.length).join("/");
 
+  const [scrollY, setScrollY] = useState(0);
+
   const handleChange = (value: string) => {
+    setScrollY(window.scrollY); // Запоминаем позицию перед сменой языка
     router.replace(`/${value}/${slug}`);
   };
+
+  useEffect(() => {
+    window.scrollTo(0, scrollY); // Восстанавливаем позицию после рендера
+  }, [scrollY]);
 
   return (
     <Select
       value={params.locale as string}
       onValueChange={(value) => handleChange(value)}
     >
-      <SelectTrigger className="max-w-[75px] bg-inherit border-none font-bold text-white   focus:ring-0 flex">
+      <SelectTrigger className="max-w-[75px] bg-inherit border-none font-bold text-white focus:ring-0 flex">
         <SelectValue placeholder={params.locale} />
       </SelectTrigger>
       <SelectContent className="text-[#640000] font-bold">
