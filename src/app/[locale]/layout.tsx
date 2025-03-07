5887f71fc73305dd31f006e2f0bc896f48c66c68
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2025-03-07 11:20:17 +0500

    change: Шрифт Arial вместо Raleway

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,3 @@
-import { Raleway } from "next/font/google";
 import Script from "next/script";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
@@ -7,18 +6,6 @@ import { QueryProvider } from "@/shared/providers";
 import { Toaster } from "@/shared/ui";
 
 import "../globals.css";
-
-// const inter = Montserrat({ subsets: ["latin"] });
-const raleway = Raleway({
-  weight: ["400"],
-  variable: "--font-raleway",
-  subsets: ["cyrillic", "latin"],
-});
-// const montserrat = Montserrat({
-//   weight: ["400", "700", "900"],
-//   variable: "--font-montserrat",
-//   subsets: ["cyrillic", "latin"],
-// });
 
 export default async function LocaleLayout({
   children,
@@ -37,12 +24,12 @@ export default async function LocaleLayout({
         <Script src="https://maps.api.2gis.ru/2.0/loader.js?pkg=full" />
         <Script src="../../shared/lib/map.js" />
       </head>
-      <body className={`${raleway.className}`}>
+      <body >
         <NextIntlClientProvider messages={messages}>
           <QueryProvider>{children}</QueryProvider>
           <Toaster />
         </NextIntlClientProvider>
       </body>
-    </html>
+    </html >
   );
 }
