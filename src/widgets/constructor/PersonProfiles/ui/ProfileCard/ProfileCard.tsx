74475660613e6cd14f15b96fec8d043f71d20b05
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2025-05-16 12:59:04 +0500

    fix: Персональные профили на разных языках

--- src/widgets/constructor/PersonProfiles/ui/ProfileCard/ProfileCard.tsx
+++ src/widgets/constructor/PersonProfiles/ui/ProfileCard/ProfileCard.tsx
@@ -12,13 +12,15 @@ export const ProfileCard = ({ locale, content }: { locale: string, content: any 
 
     const t = useTranslations()
 
+      const filePath =
+            content?.image?.[locale] || content?.image?.ru;
 
     return (
         <Modal modalSlot={body}>
             <section className="flex px-5 cursor-pointer  flex-col  gap-2 shadow-[0_0_30px_0px_rgba(0,0,0,0.05)] rounded-2xl bg-white mt-4 ">
                 <section className="relative max-w-[400px] w-full h-[363px] sm:mx-auto">
                     <Image
-                        src={`${backendImageUrl}${content.image}`}
+                        src={`${backendImageUrl}${filePath}`}
                         fill
                         priority
                         objectFit="cover"
