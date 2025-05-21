f4adb5c3ec5a48888d222d8f4b18020944c29f73
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2025-05-21 15:21:36 +0500

    upd: Перевод в главной странице блока ielts

--- src/features/home/ui/Ielts.tsx
+++ src/features/home/ui/Ielts.tsx
@@ -1,3 +1,6 @@
+"use client"
+import { useTranslations } from "next-intl";
+
 import { Container, Heading } from "@/shared/ui";
 
 const ielts = [
@@ -5,9 +8,11 @@ const ielts = [
   { img: "/icons/ielts-2.jpg", link: "https://ielts.idp.com/book" },
 ];
 export const IELTS = () => {
+  const t = useTranslations("home");
+
   return (
     <Container>
-      <Heading className="text-center">Экзамен IELTS</Heading>
+      <Heading className="text-center">{t("ielts.exam")}</Heading>
       <section className="mt-4 flex flex-col items-center lg:flex-row md:justify-center gap-10">
         {ielts.map((i) => (
           <a href={i.link} target="_blank" key={i.link}>
