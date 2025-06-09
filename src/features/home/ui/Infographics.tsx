570698e3161c4decf47ecc1cbd36a2680aa74334
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-06-09 13:47:22 +0500

    update: infographics

--- src/features/home/ui/Infographics.tsx
+++ src/features/home/ui/Infographics.tsx
@@ -10,38 +10,34 @@ export const Infographics = () => {
 
   return (
     <Container>
-      <Heading className="mb-4 md:mb-8">
+      <Heading className="mb-4 md:mb-8 text-center text-[#67493E] text-calc-2xl">
         <>{t("infographics.title")}</>
       </Heading>
-      <section className="flex justify-center flex-wrap xl:flex-nowrap gap-2   md:gap-5  rounded-md ">
+      <section className="flex flex-wrap justify-center box-border gap-10">
         {Array.from({ length: 5 })
           .fill(1)
-          .map((s, idx) => (
-            <section
-              key={idx}
-              className="flex items-center relative h-[5.75rem] md:h-[10.75rem] rounded-md px-2.5 w-[17.875rem] bg-abu_primary overflow-hidden"
-            >
-              <span className="text-white font-bold text-calc-xl">
-                {t(`infographics.${idx + 1}`)}
-              </span>
-              <Image
-                className={clsx(
-                  "absolute left-1/2 -bottom-10   h-auto",
-                  {
-                    1: "w-[9.188rem]",
-                    2: "w-[5.688rem]",
-                    3: "w-[8.25rem]",
-                    4: "w-[10.5rem]",
-                    5: "w-[10.375rem]",
-                  }[idx + 1]
-                )}
-                src={`/infograph/${idx + 1}.svg`}
-                alt={`index-${idx + 1}`}
-                width={100}
-                height={100}
-              />
-            </section>
-          ))}
+          .map((s, idx) => {
+            return (
+              <div
+                key={idx}
+                className={`mb-4 relative rounded-3xl p-[2px] bg-white border bg-gradient-to-tr from-[#67493E] to-[#FFBE0B] `}
+              >
+                <div className="bg-white rounded-[22px] p-6">
+                  <div className="relative w-24 h-24 bg-[#FDF3DF] p-4 rounded-full flex items-center justify-center mx-auto">
+                    <Image
+                      src={`/infograph/${idx + 1}.svg`}
+                      alt={`index-${idx + 1}`}
+                      fill
+                      className="object-contain p-3"
+                    />
+                  </div>
+                  <p className="text-center text-calc-xl font-semibold text-gray-800">
+                    50+ білім беру бағдарламасы
+                  </p>
+                </div>
+              </div>
+            );
+          })}
       </section>
     </Container>
   );
