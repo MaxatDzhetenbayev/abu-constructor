56dd8c137f7eb793d291b36cd5459d70cc775b91
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-06-20 09:49:30 +0500

    upd: Галлерея

--- src/widgets/constructor/Gallery/Gallery.tsx
+++ src/widgets/constructor/Gallery/Gallery.tsx
@@ -38,46 +38,49 @@ function Gallery({
           <SwiperGallery contents={contents} />
         ) : (
           <>
-            {contents.map(({ content }, idx) => (
-              <Dialog key={idx}>
-                <DialogTrigger
-                  className={clsx(
-                    "cursor-pointer flex-1 min-w-[300px] w-[100%] [@media(max-width:390px)]:h-[300px] h-[350px] rounded-md bg-slate-50"
-                  )}
-                  style={{
-                    backgroundImage: `url('${backendImageUrl}${content.image}')`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                  }}
-                ></DialogTrigger>
-                <DialogContent className=" rounded-md min-w-[calc(100vw-100px)] h-auto  ">
-                  <DialogHeader>
-                    <DialogTitle className="opacity-0">
-                      Gallery modal
-                    </DialogTitle>
-                    <DialogDescription className="opacity-0">
-                      There you can see selected image
-                    </DialogDescription>
-                  </DialogHeader>
-                  <div className="flex items-center justify-center">
-                    <div
-                      className={clsx(
-                        "relative w-[290px] h-[290px] sm:w-[320px] sm:h-[320px] md:w-[420px] md:h-[420px]  lg:w-[520px] lg:h-[520px]"
-                      )}
-                    >
-                      <Image
-                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                        className="w-full h-full object-cover rounded-md"
-                        fill
-                        quality={80}
-                        src={`${backendImageUrl}/${content.image}`}
-                        alt=""
-                      />
+            {contents.map(({ content }, idx) => {
+              const filePath = content?.image?.[locale] || content?.image?.ru;
+              return (
+                <Dialog key={idx}>
+                  <DialogTrigger
+                    className={clsx(
+                      "cursor-pointer flex-1 min-w-[300px] w-[100%] [@media(max-width:390px)]:h-[300px] h-[350px] rounded-md bg-slate-50"
+                    )}
+                    style={{
+                      backgroundImage: `url('${backendImageUrl}${content.image}')`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                    }}
+                  ></DialogTrigger>
+                  <DialogContent className=" rounded-md min-w-[calc(100vw-100px)] h-auto  ">
+                    <DialogHeader>
+                      <DialogTitle className="opacity-0">
+                        Gallery modal
+                      </DialogTitle>
+                      <DialogDescription className="opacity-0">
+                        There you can see selected image
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="flex items-center justify-center">
+                      <div
+                        className={clsx(
+                          "relative w-[290px] h-[290px] sm:w-[320px] sm:h-[320px] md:w-[420px] md:h-[420px]  lg:w-[520px] lg:h-[520px]"
+                        )}
+                      >
+                        <Image
+                          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                          className="w-full h-full object-cover rounded-md"
+                          fill
+                          quality={80}
+                          src={`${backendImageUrl}${filePath}`}
+                          alt=""
+                        />
+                      </div>
                     </div>
-                  </div>
-                </DialogContent>
-              </Dialog>
-            ))}
+                  </DialogContent>
+                </Dialog>
+              );
+            })}
           </>
         )}
       </section>
