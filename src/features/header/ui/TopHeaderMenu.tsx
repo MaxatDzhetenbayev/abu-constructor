900b43a772e378089a4003ecbb562eb61675a164
yekinsgn <yekinsgn@yekinsgns-MacBook-Air.local>
2025-07-02 11:14:17 +0500

    upd: Смена ссылки на старый сайт

--- src/features/header/ui/TopHeaderMenu.tsx
+++ src/features/header/ui/TopHeaderMenu.tsx
@@ -1,34 +1,34 @@
-"use client";
-import Link from "next/link";
-import { useTranslations } from "next-intl";
-import React from "react";
+'use client'
+import Link from 'next/link'
+import { useTranslations } from 'next-intl'
+import React from 'react'
 
-import { topHeaderMenuList } from "@/shared/config/topHeaderMenu";
+import { topHeaderMenuList } from '@/shared/config/topHeaderMenu'
 
 export const TopHeaderMenu = () => {
-  const t = useTranslations();
+    const t = useTranslations()
 
-  return (
-    <ul
-      className="text-white items-center hidden [@media(min-width:890px)]:flex py-3"
-      style={{ gap: "clamp(10px, 1.5vw, 15px)" }}
-    >
-      <Link className="text-[#67493E] font-bold" href="https://abu.edu.kz/">
-        {t("old_version")}↗
-      </Link>
+    return (
+        <ul
+            className="text-white items-center hidden [@media(min-width:890px)]:flex py-3"
+            style={{ gap: 'clamp(10px, 1.5vw, 15px)' }}
+        >
+            <Link className="text-[#67493E] font-bold" href="https://old.abu.edu.kz/">
+                {t('old_version')}↗
+            </Link>
 
-      {topHeaderMenuList.map((item, index) => (
-        <li key={index}>
-          <Link
-            href={item.link}
-            key={index}
-            target="_blank"
-            className="text-[#67493E] font-bold before:content-['•'] before:mr-4"
-          >
-            {item.title}↗
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-};
+            {topHeaderMenuList.map((item, index) => (
+                <li key={index}>
+                    <Link
+                        href={item.link}
+                        key={index}
+                        target="_blank"
+                        className="text-[#67493E] font-bold before:content-['•'] before:mr-4"
+                    >
+                        {item.title}↗
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    )
+}
