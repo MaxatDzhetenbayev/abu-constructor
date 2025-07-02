d03f8b2eb06c469c2c9bfb549411594d4d1c4bbb
iufb <bashirov3ld2@gmail.com>
2025-07-02 19:20:10 +0500

    fix: files + mobile change locale

--- src/features/ChangeLocale.tsx
+++ src/features/ChangeLocale.tsx
@@ -1,37 +1,40 @@
 "use client";
+
 import { useParams, usePathname, useRouter } from "next/navigation";
+import { ButtonHTMLAttributes } from "react";
 
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
 } from "@/shared/ui";
+import clsx from "clsx";
 
-export const ChangeLocale = () => {
-  const params = useParams();
-  const router = useRouter();
-  const path = usePathname().split("/");
-  const slug = path.slice(2, path.length).join("/");
+export const ChangeLocale = ({ className, ...props }: ButtonHTMLAttributes<HTMLButtonElement>) => {
+    const params = useParams();
+    const router = useRouter();
+    const path = usePathname().split("/");
+    const slug = path.slice(2, path.length).join("/");
 
-  const handleChange = (value: string) => {
-    router.replace(`/${value}/${slug}`);
-  };
+    const handleChange = (value: string) => {
+        router.replace(`/${value}/${slug}`);
+    };
 
-  return (
-    <Select
-      value={params.locale as string}
-      onValueChange={(value) => handleChange(value)}
-    >
-      <SelectTrigger className="max-w-[75px] bg-inherit border-none font-bold text-[#67493E]   focus:ring-0 flex">
-        <SelectValue placeholder={params.locale} />
-      </SelectTrigger>
-      <SelectContent className="text-[#640000] font-bold">
-        <SelectItem value="ru">RU</SelectItem>
-        <SelectItem value="kz">KZ</SelectItem>
-        <SelectItem value="en">EN</SelectItem>
-      </SelectContent>
-    </Select>
-  );
+    return (
+        <Select
+            value={params.locale as string}
+            onValueChange={(value) => handleChange(value)}
+        >
+            <SelectTrigger className={clsx("max-w-[75px] bg-inherit border-none font-bold text-[#67493E]   focus:ring-0 flex", className)} {...props}>
+                <SelectValue placeholder={params.locale} />
+            </SelectTrigger>
+            <SelectContent className="text-[#640000] font-bold">
+                <SelectItem value="ru">RU</SelectItem>
+                <SelectItem value="kz">KZ</SelectItem>
+                <SelectItem value="en">EN</SelectItem>
+            </SelectContent>
+        </Select>
+    );
 };
