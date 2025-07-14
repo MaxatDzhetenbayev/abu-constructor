843ad3a04680d805b0a144a70a5015de39218563
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-07-14 16:40:19 +0500

    upd: Новые статические ссылки в навигации

--- src/features/header/ui/TopHeaderMenu.tsx
+++ src/features/header/ui/TopHeaderMenu.tsx
@@ -1,32 +1,33 @@
-'use client'
-import clsx from 'clsx'
-import Link from 'next/link'
-import { useTranslations } from 'next-intl'
+"use client";
+import clsx from "clsx";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
 
-import { topHeaderMenuList } from '@/shared/config/topHeaderMenu'
+import { topHeaderMenuList } from "@/shared/config/topHeaderMenu";
 
 export const TopHeaderMenu = () => {
-    const t = useTranslations()
+  const t = useTranslations("top_menu");
 
-    return (
-        <ul
-            className="text-white items-center hidden [@media(min-width:890px)]:flex py-3"
-            style={{ gap: 'clamp(10px, 1.5vw, 15px)' }}
-        >
-
-
-            {topHeaderMenuList.map((item, index) => (
-                <li key={index}>
-                    <Link
-                        href={item.link}
-                        key={index}
-                        target="_blank"
-                        className={clsx("text-[#67493E] font-bold  ", index !== 0 && "before:content-['•'] before:mr-4")}
-                    >
-                        {index == 0 ? t(item.title) : item.title}↗
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    )
-}
+  return (
+    <ul
+      className="text-white items-center hidden [@media(min-width:890px)]:flex py-3"
+      style={{ gap: "clamp(10px, 1.5vw, 15px)" }}
+    >
+      {topHeaderMenuList.map((item, index) => (
+        <li key={index}>
+          <Link
+            href={item.link}
+            key={index}
+            target="_blank"
+            className={clsx(
+              "text-[#67493E] font-bold  ",
+              index !== 0 && "before:content-['•'] before:mr-4"
+            )}
+          >
+            {index == 0 ? t(item.title) : item.title}↗
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
--- src/shared/config/topHeaderMenu.ts
+++ src/shared/config/topHeaderMenu.ts
@@ -1,11 +1,11 @@
 export const topHeaderMenuList = [
-    //   { title: "Обучающимся", link: "https://education.abu.edu.kz/" },
-    //   { title: "Международное сотрудничество", link: "home" },
-    //   { title: "Выпусникам", link: "home" },
-    //
-
-    { title: "old_version", link: "https://old.abu.edu.kz/" },
-    { title: "AIS", link: "https://ais.semuniver.kz/login.php" },
-    { title: "Abai IT", link: "https://abai-it.kz/" },
-    { title: "Abai IT School", link: "https://abai-it-school.abu.edu.kz/" },
+  { title: "old_version", link: "https://old.abu.edu.kz/" },
+  { title: "AIS", link: "https://ais.semuniver.kz/login.php" },
+  { title: "Abai IT", link: "https://abai-it.kz/" },
+  { title: "Abai IT School", link: "https://abai-it-school.abu.edu.kz/" },
+  { title: "abu_life", link: "https://abu-life.abu.edu.kz" },
+  {
+    title: "international",
+    link: "https://international.abu.edu.kz",
+  },
 ];
