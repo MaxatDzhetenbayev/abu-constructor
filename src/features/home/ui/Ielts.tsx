51deb76b3d192d483844daa5a1e55bd47846caec
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-07-14 16:30:22 +0500

    upd: Блок Проекты

--- src/features/home/ui/Ielts.tsx
+++ src/features/home/ui/Ielts.tsx
@@ -6,16 +6,25 @@ import { Container, Heading } from "@/shared/ui";
 const ielts = [
   { img: "/icons/ielts-1.jpg", link: "https://ielts.kz/" },
   { img: "/icons/ielts-2.jpg", link: "https://ielts.idp.com/book" },
+  {
+    img: "/icons/ielts-3.jpg",
+    link: "https://circulec.tguk.tj/view_article.php?id=15",
+  },
 ];
 export const IELTS = () => {
   const t = useTranslations("home");
 
   return (
     <Container>
-      <Heading>{t("ielts.exam")}</Heading>
+      <Heading>{t("ielts.project")}</Heading>
       <section className="mt-4 flex flex-col items-center lg:flex-row md:justify-center gap-10">
         {ielts.map((i) => (
-          <a href={i.link} target="_blank" key={i.link} className="flex-1 w-full">
+          <a
+            href={i.link}
+            target="_blank"
+            key={i.link}
+            className="flex-1 w-full"
+          >
             <img src={i.img} alt="ielts" className="" />
           </a>
         ))}
