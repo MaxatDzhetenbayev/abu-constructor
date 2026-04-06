78a2ac7edd1725fe60ae8544d0983506bea4fd70
MaxatDzhetenbayev <maxat.dzhetenbaev@gmail.com>
2026-04-06 12:26:21 +0500

    refactor: enhance sitemap generation by adding XML escaping for slugs and improving navigation handling

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,33 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from 'next'
 
-import { fetchNavigation } from '@/entities/navigation/api/fetchNavigation';
+import { fetchNavigation } from '@/entities/navigation/api/fetchNavigation'
 
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const DOMAIN = process.env.SITE_URL ?? 'https://abu.edu.kz'
-
-  const navigation = await fetchNavigation();
+  const navigation = await fetchNavigation()
   const locales = ['ru', 'kz', 'en']
 
-  return navigation?.length ? navigation.flatMap(item =>
-    locales.map(locale => {
-      const path = item.slug ? `/${locale}/${item.slug}` : `/${locale}`
-  
-      return {
-        url: `${DOMAIN}${path}`,
-        lastModified: new Date(item.updatedAt),
-      }
-    })
-  ) : []
+  return navigation?.length
+    ? navigation.flatMap(item =>
+        locales.map(locale => {
+          const slug = item.slug ? escapeXml(item.slug) : ''
+          const path = slug ? `/${locale}/${slug}` : `/${locale}`
+
+          return {
+            url: `${DOMAIN}${path}`,
+            lastModified: new Date(item.updatedAt),
+          }
+        })
+      )
+    : []
+}
+
+function escapeXml(str: string) {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;')
 }
